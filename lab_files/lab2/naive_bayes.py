eaa3f43819beaeccb9abfa936610686602de0e74
Kujonick <przemek.r@opoczta.pl>
2025-03-14 15:53:05 +0100

    first two labs

--- lab_files/lab2/naive_bayes.py
+++ lab_files/lab2/naive_bayes.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from collections import defaultdict
+import re
 
 class NaiveBayes():
     """
@@ -38,10 +40,39 @@ class NaiveBayes():
         :return: None
         """
         # 1. Count number of ham and spam emails
-        # 2. Reinitialize word count dictionaries
-        # 3. Process each email in the training set
-        raise NotImplementedError("Fit method not implemented") # Delete this line when you implement the method
+        counts = train_df['label'].value_counts()
+        self.num_train_hams = counts[self.HAM_LABEL]
+        self.num_train_spams = counts[self.SPAM_LABEL]
 
+        # 2. Reinitialize word count dictionaries
+        self.word_counts_spam = defaultdict(lambda : 1)
+        self.word_counts_ham = defaultdict(lambda : 1)
+
+        # 3. Process each email in the training set
+
+        self.sum_words_ham = 0
+        self.sum_words_spam = 0
+        for i, row in train_df.iterrows():
+            words = self.get_word_set(row['email'])
+
+            if row['label'] == self.HAM_LABEL:
+                target = self.word_counts_ham  
+            else:
+                target = self.word_counts_spam
+            counter = 0 
+                
+            for word in words:
+                if len(word) < 2:
+                    continue
+                counter += 1
+                target[word] += 1
+
+            if row['label'] == self.HAM_LABEL:
+                self.sum_words_ham += counter
+            else:
+                self.sum_words_spam += counter
+        
+                
     # TODO implement:
     def predict(self, email_text: str) -> str:
         """
@@ -51,11 +82,29 @@ class NaiveBayes():
         :return: The predicted label ('ham' or 'spam').
         """
         # 1. Get words in the email
+        words = self.get_word_set(email_text)
+
         # 2. Calculate prior probabilities
+        spam_prob = self.num_train_spams / (self.num_train_hams + self.num_train_spams)
+        ham_prob = self.num_train_hams / (self.num_train_hams + self.num_train_spams)
+        
         # 3. Calculate log probabilities for ham and spam (for computational reasons)
+        log_prob_spam = np.log(spam_prob)
+        log_prob_ham = np.log(ham_prob)
+
         # 4. For each word in the email, update the log probabilities
-        # 4. Predict label (in case of a tie, return HAM_LABEL)
-        raise NotImplementedError("Fit method not implemented") # Delete this line when you implement the method
+        for word in words:
+            if len(word) < 2:
+                continue
+            log_prob_spam += np.log(self.word_counts_spam[word] / self.num_train_spams)
+            log_prob_ham += np.log(self.word_counts_ham[word] / self.num_train_hams)
+        
+
+        # 5. Predict label (in case of a tie, return HAM_LABEL)
+        if log_prob_ham >= log_prob_spam:
+            return self.HAM_LABEL
+        return self.SPAM_LABEL
+    
     
     def predict_df(self, df: pd.DataFrame) -> pd.Series:
         """
@@ -85,3 +134,37 @@ class NaiveBayes():
                 correct += 1
                 
         return correct / total
+    
+    
+class MultinomialBayes(NaiveBayes):
+    def get_word_set(self, email_text: str) -> set:
+        # Clean the text and split into words
+        text = email_text.replace('\r', '').replace('\n', ' ')
+        words = text.split(' ')
+        return words
+    
+    def predict(self, email_text: str) -> str:
+
+        # 1. Get words in the email
+        words = self.get_word_set(email_text)
+
+        # 2. Calculate prior probabilities
+        spam_prob = self.num_train_spams / (self.num_train_hams + self.num_train_spams)
+        ham_prob = self.num_train_hams / (self.num_train_hams + self.num_train_spams)
+        
+        # 3. Calculate log probabilities for ham and spam (for computational reasons)
+        log_prob_spam = np.log(spam_prob)
+        log_prob_ham = np.log(ham_prob)
+
+        # 4. For each word in the email, update the log probabilities
+        for word in words:
+            if len(word) < 2:
+                continue
+            log_prob_spam += np.log(self.word_counts_spam[word] / self.sum_words_spam)
+            log_prob_ham += np.log(self.word_counts_ham[word] / self.sum_words_ham)
+        
+
+        # 5. Predict label (in case of a tie, return HAM_LABEL)
+        if log_prob_ham >= log_prob_spam:
+            return self.HAM_LABEL
+        return self.SPAM_LABEL
